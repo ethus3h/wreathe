c48eda22b6a1856aacc22a5c04a219ee3d7a1bf1
ethus3h <kolubat@gmail.com>
2017-05-09 08:44:53 -0400

    Update Ember tree

---
+++ prefs.js
@@ -0,0 +1,16 @@
+pref("app.update.enabled", false);
+pref("app.update.autoInstallEnabled", false);
+pref("browser.display.use_system_colors",   true);
+pref("intl.locale.matchOS", true);
+pref("general.useragent.locale", "chrome://global/locale/intl.properties");
+pref("mail.shell.checkDefaultClient", false);
+pref("calendar.integration.notify", false);
+pref("mailnews.start_page.enabled", false);
+pref("mailnews.start_page.override_url", "");
+pref("mailnews.start_page.url", "");
+pref("mail.cloud_files.enabled", false);
+pref("mail.provider.enabled", false);
+// Make clicking calendar divider not close calendar
+// Have POP3 be mail type selected by default
+// Hide/rename Enigmail menu item
+// Use system theme
