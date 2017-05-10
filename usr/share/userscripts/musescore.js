aeb3b9a41cef23b4ae6e24c5b7ce377c46dcd0eb
Elliot Chandler <elliot@futuramerlin.com>
2017-05-09 21:36:35 -0400

    Update musescore.js

--- usr/share/userscripts/musescore.js
+++ usr/share/userscripts/musescore.js
@@ -3,49 +3,49 @@
 // @namespace      http://futuramerlin.com/
 // @description    Fixes the download links on MuseScore.com so they don't try to slurp up private information.
 // @include        *musescore.com/user/*
+// @grant          none
 // @version        0.0.1
 // @downloadURL    https://github.com/ethus3h/wreathe/raw/master/usr/share/userscripts/musescore.js
 // ==/UserScript==
 
 // URLs look like: 
 // http://static.musescore.com/(score-ID)/(download-key)/score.ext
-alert("blah");
+
 // Get the download key
-var imageElement = Document.querySelector('.viewer > img');
+var imageElement = document.querySelector('.viewer img');
 var imageUrl = imageElement.getAttribute('src');
 // Image URL looks like:
 // https://s3.amazonaws.com/static.musescore.com/3871256/4ccbadbc7b/score_0.png?no-cache=1494248226
-var patt = ".+\/([a-f0-9]{10})\/.+";
+var patt = new RegExp(".+\/([a-f0-9]{10})\/.+");
 var key = imageUrl.replace(patt, '$1');
 
 // Get a list of download buttons to fix
-var dlButtons = [];
-var foundLastElement = false;
-var elementCounter = 0;
-while (!foundLastElement)
-{
-    var foundElement = Document.querySelector('#downloadModal > h5:nth-of-type('.concat(elementCounter, ')'));
-    if (foundElement != null)
-    {
-        dlButtons.push(foundElement);
-    }
-    else
-    {
-        foundLastElement = true;
-    }
-}
+var dlButtons = document.querySelectorAll('#download-modal h5 > a');
 
-for (var i = 0; i < elementCounter; i++)
+for (var i = 0; i < dlButtons.length; i++)
 {
     button = dlButtons[i];
     // Button URL looks like:
-    // /score/3871256/download/pdf
+    // /score/3871256/download/pdf or
+    // /score/3871256/download
     buttonUrl = button.getAttribute('href');
+
     // Get the file extension
-    var patt = new RegExp("\w+$");
-    var ext = patt.exec(buttonUrl);
+    if (buttonUrl.match(new RegExp("download$")))
+    {
+        var ext = "mscz";
+    }
+    else
+    {
+       var patt = new RegExp('.+\/(\w+)$');
+       var ext = buttonUrl.replace(patt, '$1');
+    }
+    alert(ext);
+
     // Get the score ID
     var patt = new RegExp("\d+");
     var id = patt.exec(buttonUrl);
+    
+    // Put together the fixed URL and set it as the button's target
     button.setAttribute('href', '/score/'.concat(id, '/download/', ext));
 }
