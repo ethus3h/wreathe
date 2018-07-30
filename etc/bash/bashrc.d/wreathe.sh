a7ed0ca801ad21e84832dc0708546a7ffa68a0d9
ethus3h <kolubat@gmail.com>
2018-07-30 01:00:23 -0400

    Update Git repository using egup-single

--- etc/bash/bashrc.d/wreathe.sh
+++ etc/bash/bashrc.d/wreathe.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 source ember_bash_setup
 
-if { ! ((EUID)) } && -f ~/.bash_history.keep-public-mirror; then
-    cp ~/.bash_history ~/.bash_history.public-mirror
-    chmod +r ~/.bash_history.public-mirror
-    emdate > ~/.bash_history.emdate
+if ! ((EUID)); then
+    if -f ~/.bash_history.keep-public-mirror; then
+        cp ~/.bash_history ~/.bash_history.public-mirror
+        chmod +r ~/.bash_history.public-mirror
+        emdate > ~/.bash_history.emdate
+    fi
 fi
 
 # eval $(thefuck --alias)
