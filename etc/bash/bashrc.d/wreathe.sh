4453f58f04d342c0a7470173bd5745fe3b30652c
ethus3h <kolubat@gmail.com>
2020-04-08 19:06:54 -0400

    Update Git repository using egup-single

--- etc/bash/bashrc.d/wreathe.sh
+++ etc/bash/bashrc.d/wreathe.sh
@@ -11,13 +11,11 @@ export INFOPATH="$HOME/.config/guix/current/share/info:$INFOPATH"
 
 if ! ((EUID)); then
     if [[ -f ~/.bash_history.keep-public-mirror ]]; then
-	mkdir -p /.bash_history.public-mirror-dir
-	chmod +rx /.bash_history.public-mirror-dir
-        cp ~/.bash_history /.bash_history.public-mirror-dir/.bash_history.public-mirror
-        chmod +r /.bash_history.public-mirror-dir/.bash_history.public-mirror
-        emdate > /.bash_history.public-mirror-dir/.bash_history.emdate
-	chmod +r /.bash_history.public-mirror-dir/.bash_history.emdate
+        mkdir -p /.bash_history.public-mirror-dir
+        chmod +rx /.bash_history.public-mirror-dir
+            cp ~/.bash_history /.bash_history.public-mirror-dir/.bash_history.public-mirror
+            chmod +r /.bash_history.public-mirror-dir/.bash_history.public-mirror
+            emdate > /.bash_history.public-mirror-dir/.bash_history.emdate
+        chmod +r /.bash_history.public-mirror-dir/.bash_history.emdate
     fi
 fi
-
-# eval $(thefuck --alias)
