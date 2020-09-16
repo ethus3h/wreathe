24a6154358c6f3210420eb38d087dbcba72b4181
Elli Chandler <elli@futuramerlin.com>
2020-09-16 01:12:32 -0400

    Update Git repository using egup-single

--- etc/bash/bashrc.d/wreathe.sh
+++ etc/bash/bashrc.d/wreathe.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 source ember_bash_setup
-    export GUIX_PROFILE="$HOME/.config/guix/current"
-    source "$GUIX_PROFILE/etc/profile"
-    export GUIX_PROFILE="$HOME/.guix-profile"
-    source "$GUIX_PROFILE/etc/profile"
+export GUIX_PROFILE="$HOME/.config/guix/current"
+source "$GUIX_PROFILE/etc/profile"
+export GUIX_PROFILE="$HOME/.guix-profile"
+source "$GUIX_PROFILE/etc/profile"
 export GUIX_LOCPATH="$GUIX_PROFILE/lib/locale"
 export PATH="$GUIX_PROFILE/bin:$PATH"
 export INFOPATH="$HOME/.config/guix/current/share/info:$INFOPATH"
